f010d5ae4e81daa6096ec0f6bf71a8ffbc90220b
Pier <piergianni.serra@gmail.com>
2013-03-06 16:03:30 +0100

    in c#
    riesci a vederlo?

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Liberare le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.lstNetworks = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.posizione = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(260, 227);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "refresh";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lstNetworks
+            // 
+            this.lstNetworks.Location = new System.Drawing.Point(12, 62);
+            this.lstNetworks.Name = "lstNetworks";
+            this.lstNetworks.Size = new System.Drawing.Size(664, 97);
+            this.lstNetworks.TabIndex = 1;
+            this.lstNetworks.UseCompatibleStateImageBehavior = false;
+            this.lstNetworks.View = System.Windows.Forms.View.Details;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "DIMMI DOVE SEI:";
+            // 
+            // posizione
+            // 
+            this.posizione.Location = new System.Drawing.Point(127, 19);
+            this.posizione.Name = "posizione";
+            this.posizione.Size = new System.Drawing.Size(100, 20);
+            this.posizione.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(370, 232);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(238, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "PER SALVARE LE INFO DEVI FARE REFRESH";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(688, 262);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.posizione);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lstNetworks);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "lstNetworks";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView lstNetworks;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox posizione;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,124 @@
+﻿using NativeWifi;
+
+
+using System;
+using System.Timers;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        System.Timers.Timer aTimer = new System.Timers.Timer();
+        
+        public Form1()
+        {
+            InitializeComponent();
+            aTimer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
+            aTimer.Interval = 5000;
+            lstNetworks.Columns.Add("Nome",100);
+            lstNetworks.Columns.Add("Crittografia", 100);
+            lstNetworks.Columns.Add("Potenza", 100);
+            lstNetworks.Columns.Add("dot11BssType", 100);
+            lstNetworks.Columns.Add("dot11DefaultAuthAlgorithm", 100);
+            lstNetworks.Columns.Add("dot11DefaultCipherAlgorithm", 100);
+            lstNetworks.Columns.Add("flag", 100);
+            lstNetworks.Columns.Add("GetHashCode", 100);
+            lstNetworks.Columns.Add("GetType", 100);
+            lstNetworks.Columns.Add("morePhyTypes", 100);
+            lstNetworks.Columns.Add("networkConnectable", 100);
+            lstNetworks.Columns.Add("numberOfBssids", 100);
+            lstNetworks.Columns.Add("profileName", 100);
+            lstNetworks.Columns.Add("securityEnabled", 100);
+            lstNetworks.Columns.Add("wlanNotConnectableReason", 100);
+        }
+
+        private void button1_Click(object sender, System.EventArgs e)
+        {
+            aTimer.Enabled = true;
+        }
+
+        private void OnTimedEvent(object source, ElapsedEventArgs e)
+        {
+            lstNetworks.Items.Clear();
+
+            WlanClient client = new WlanClient();
+            String testo1 = "--->Cattura fatta in: " + posizione.Text + " in data " + System.DateTime.Now.ToString() + "\r\n";
+            if (posizione.Text == "")
+            {
+                MessageBox.Show("inserisci il posto per proseguire");
+                return;
+            }
+            foreach (WlanClient.WlanInterface wlanIface in client.Interfaces)
+            {
+                Wlan.WlanAvailableNetwork[] networks = wlanIface.GetAvailableNetworkList(0);
+              
+                Wlan.WlanBssEntry[] wlanBssEntries = wlanIface.GetNetworkBssList();
+                foreach (Wlan.WlanBssEntry wlanBssEntry in wlanBssEntries)
+                {
+
+
+                    
+                    byte[] macAddr = wlanBssEntry.dot11Bssid;
+                    var macAddrLen = (uint)macAddr.Length;
+                    var str = new string[(int)macAddrLen];
+                    for (int i = 0; i < macAddrLen; i++)
+                    {
+                        str[i] = macAddr[i].ToString("x2");
+                    }
+                    string mac = string.Join("", str);
+                    mac = " wlanBssEntry: " + wlanBssEntry.dot11Ssid.ToString() + " dot11BssPhyType: " + wlanBssEntry.dot11BssType.ToString() + " dot11BssPhyType: " + wlanBssEntry.dot11BssPhyType.ToString() + " frequenza khz " + wlanBssEntry.chCenterFrequency.ToString() + " capabilityInformation " + wlanBssEntry.capabilityInformation.ToString() + " beaconPeriod: " + wlanBssEntry.beaconPeriod.ToString() + " MAC: " + mac + "\r\n";
+                     
+                    System.IO.File.AppendAllText("catture.txt",mac);
+                   // MessageBox.Show(mac);
+                }
+
+                //MessageBox.Show("fine");
+
+
+                foreach (Wlan.WlanAvailableNetwork network in networks)
+                {
+                    Wlan.Dot11Ssid ssid = network.dot11Ssid;
+                    string networkName = Encoding.ASCII.GetString(ssid.SSID, 0, (int)ssid.SSIDLength);
+                    //ListViewItem item = new ListViewItem(networkName);
+                    //item.SubItems.Add(network.dot11DefaultCipherAlgorithm.ToString());
+                    //item.SubItems.Add(network.wlanSignalQuality + "%");
+                    //item.SubItems.Add(network.dot11BssType.ToString());
+                    //item.SubItems.Add(network.dot11DefaultAuthAlgorithm.ToString());
+                    //item.SubItems.Add(network.dot11DefaultCipherAlgorithm.ToString());
+                    //item.SubItems.Add(network.flags.ToString());
+                    //item.SubItems.Add(network.GetHashCode().ToString());
+                    //item.SubItems.Add(network.GetType().ToString());
+                    //item.SubItems.Add(network.morePhyTypes.ToString());
+                    //item.SubItems.Add(network.networkConnectable.ToString());
+                    //item.SubItems.Add(network.numberOfBssids.ToString());
+                    //item.SubItems.Add(network.profileName.ToString());
+                    //item.SubItems.Add(network.securityEnabled.ToString());
+                    //item.SubItems.Add(network.wlanNotConnectableReason.ToString());
+
+                    string testo = testo1 + "  nome rete: " + networkName + " potenza segnale: " + network.wlanSignalQuality + "% HashCode:" + network.GetHashCode().ToString() + " Bssid: "+ network.numberOfBssids.ToString() + "\r\n";
+                    testo1 = "   ";
+                    System.IO.File.AppendAllText("catture.txt", testo);
+
+                           
+
+                    //lstNetworks.Items.Add(item);
+                }
+                client.chiudi();
+            }
+             
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
